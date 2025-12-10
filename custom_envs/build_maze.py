205c7ebb2bcab74b3b524415f7b4ebfa94c83372
xiehui <xiehui03@qq.com>
2025-12-10 14:07:18 +0800

    add_maze_init

--- custom_envs/build_maze.py
+++ custom_envs/build_maze.py
@@ -1,1 +1,17 @@
-import dm_control.locomotion.arenas as arenas
+
+import labmaze
+maze = labmaze.RandomMaze(height=11, width=13, random_seed=42)
+print(maze.entity_layer)
+
+
+MAZE_LAYOUT = """
+*********
+*********
+*********
+***   ***
+***   ***
+***   ***
+*********
+"""[1:]
+maze = labmaze.FixedMazeWithRandomGoals(entity_layer=MAZE_LAYOUT)
+print(maze.entity_layer)
